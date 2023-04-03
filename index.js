36b14fa530e73f3d20b7a2774b22d6983c44b6b5
NEWHELL69 <kushagra0304@gmail.com>
2023-04-03 19:29:09 +0530

    Exercise 3.22.

--- index.js
+++ index.js
@@ -1,169 +1,169 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors');
+const cors = require('cors')
 
 const app = express()
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.use(cors());
+app.use(cors())
 
 app.use(express.static('build'))
 
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
-    if(tokens.method(req, res) === "POST") {
-        return [
-            tokens.method(req, res),
-            tokens.url(req, res),
-            tokens.status(req, res),
-            tokens.res(req, res, 'content-length'), '-',
-            tokens['response-time'](req, res), 'ms',
-            JSON.stringify(req.body)
-        ].join(' ');
-    }
-
+  if(tokens.method(req, res) === 'POST') {
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
       tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
     ].join(' ')
+  }
+
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
 }))
 
-const myMongoose = require("./models/note.js")
+const myMongoose = require('./models/note.js')
 const Contact = myMongoose.getModel()
 
 function handleDataBaseConnection(request, response, next) {
-    if(myMongoose.getConnectionReadyState() !== 1) {
-        response.status(503).json({
-            error: "Database not connected"
-        })
-    } else {
-        next();
-    }
+  if(myMongoose.getConnectionReadyState() !== 1) {
+    response.status(503).json({
+      error: 'Database not connected'
+    })
+  } else {
+    next()
+  }
 }
 
 app.use(handleDataBaseConnection)
 
 app.get('/api/persons', (request, response, next) => {
-    Contact.find({}).then(contacts => {
-        response.json(contacts)
-    }).catch((e) => {
-        next(e)
-    })
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  }).catch((e) => {
+    next(e)
+  })
 })
 
 app.get('/info', (request, response, next) => {
-    Contact.countDocuments({}).then((count) => {
-        response.render('index', {enteriesCount: `Phonebook has info for ${count} people`, currentDate: `${new Date().toString()}` })
-    }).catch((e) => {
-        next(e)
-    })
+  Contact.countDocuments({}).then((count) => {
+    response.render('index', { enteriesCount: `Phonebook has info for ${count} people`, currentDate: `${new Date().toString()}` })
+  }).catch((e) => {
+    next(e)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    let id = request.params.id;
+  let id = request.params.id
 
-    Contact.findById(id).then((contact) => {
-        if(contact){
-            response.json(contact)
-        } else{
-            response.status(404).end()
-        }
-    }).catch((e) => {
-        next(e)
-    })
+  Contact.findById(id).then((contact) => {
+    if(contact){
+      response.json(contact)
+    } else{
+      response.status(404).end()
+    }
+  }).catch((e) => {
+    next(e)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = myMongoose.convertStringIdToObjectId(request.params.id)
+  const id = myMongoose.convertStringIdToObjectId(request.params.id)
 
-    Contact.deleteOne(id).then((obj) => {
-        if(obj.acknowledged) {
-            console.log("Deletion successfull")
-        }
-    }).catch((e) => {
-        next(e)
-    })
+  Contact.deleteOne(id).then((obj) => {
+    if(obj.acknowledged) {
+      console.log('Deletion successfull')
+    }
+  }).catch((e) => {
+    next(e)
+  })
 
-    response.status(204).end()
+  response.status(204).end()
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name) {
-        return response.status(400).json({ 
-        error: 'name missing' 
-        })
-    } else if(!body.number) {
-        return response.status(400).json({ 
-        error: 'number missing' 
-        })
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  } else if(!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
+
+  contact.save().then((newContact) => {
+    if(newContact === contact){
+      console.log(`Added ${newContact.name} number ${newContact.number} to phonebook.`)
+      response.json(newContact)
     }
-
-    const contact = new Contact({
-        name: body.name,
-        number: body.number    
-    })
-
-    contact.save().then((newContact) => {
-        if(newContact === contact){
-            console.log(`Added ${newContact.name} number ${newContact.number} to phonebook.`);
-            response.json(newContact)
-        }    
-    }).catch((e) => {
-        next(e)
-    })
+  }).catch((e) => {
+    next(e)
+  })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const id = request.params.id
+  const body = request.body
+  const id = request.params.id
 
-   if(!body.number) {
-        return response.status(400).json({ 
-            error: 'number missing' 
-        })
-    }
+  if(!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
 
-    const updationTocontact = {
-        name: body.name,
-        number: body.number    
-    }
+  const updationTocontact = {
+    name: body.name,
+    number: body.number
+  }
 
-    Contact.findByIdAndUpdate(id, updationTocontact, { new: true, runValidators: true, context: 'query' })
-        .then((newContact) => {
-            console.log(`Added ${newContact.name} number ${newContact.number} to phonebook.`);
-            
-            response.json(newContact)
-        }).catch((e) => {
-            next(e)
-        })
+  Contact.findByIdAndUpdate(id, updationTocontact, { new: true, runValidators: true, context: 'query' })
+    .then((newContact) => {
+      console.log(`Added ${newContact.name} number ${newContact.number} to phonebook.`)
+
+      response.json(newContact)
+    }).catch((e) => {
+      next(e)
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error)
+  console.error(error)
 
-    if (error.name === 'BSONError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } 
+  if (error.name === 'BSONError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted obj values provided' })
-    } 
-     
-    if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted obj values provided' })
+  }
 
-    next(error)
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
-  
+
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
--- models/note.js
+++ models/note.js
@@ -4,49 +4,49 @@ const url = process.env.MONGODB_URI
 
 console.log(typeof url)
 
-console.log("Connecting to MongoDB");
+console.log('Connecting to MongoDB')
 
-mongoose.connect(url).then((_) => {
-    console.log("Connection successfull")
+mongoose.connect(url).then(() => {
+  console.log('Connection successfull')
 }).catch((e) => {
-    console.log("Error connecting to MongoDB:", e.message)
-});
+  console.log('Error connecting to MongoDB:', e.message)
+})
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: [true, 'User name required']
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'User name required']
+  },
+  number: {
+    type: String,
+    validate:{
+      validator: function (value) {
+        return /\d{2}-\d{6,}/.test(value) || /\d{3}-\d{5,}/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate:{
-            validator: function (value) {
-                return /\d{2}-\d{6,}/.test(value) || /\d{3}-\d{5,}/.test(value)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: [true, 'User phone number required']
-    }
+    required: [true, 'User phone number required']
+  }
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject, options) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 exports.convertStringIdToObjectId = (id) => {
-    return new mongoose.Types.ObjectId(id);
+  return new mongoose.Types.ObjectId(id)
 }
 
 exports.getModel = () => {
-    return mongoose.model('Contact', contactSchema);
+  return mongoose.model('Contact', contactSchema)
 }
 
 exports.getConnectionReadyState = () => {
-    return mongoose.connection.readyState;
+  return mongoose.connection.readyState
 }
 
